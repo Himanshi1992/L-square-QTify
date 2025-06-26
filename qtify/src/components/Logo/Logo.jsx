5606f641a91b4e23f341589a2b6737acfa0b1eb6
Himanshi1992 <sonih1992@gmail.com>
2025-06-26 09:16:09 +0530

    changes to render logo image

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import LogoImage from "../../assests/logo.png";
+import logo from "../../assests/logo.png";
 
 export default function Logo() {
-  return <img src={LogoImage} alt="logo" width={67} />;
+  return <img src={logo} alt="logo" width={67} />;
 }
