4e1704423c314703c46f8791845af9482774a962
Himanshi1992 <sonih1992@gmail.com>
2025-06-26 19:53:29 +0530

    test cases

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import heroImage from "../../assets/hero_headphones.png";
 function Hero() {
   return (
     <div className={styles.hero}>
-      <div className={styles.heroText}>
+        <div>
         <h1>100 Thousand Songs, ad-free</h1>
         <p>Over thousands podcast episodes</p>
       </div>
@@ -15,7 +15,8 @@ function Hero() {
         alt="headphones"
         className={styles.heroImage}
       />
-    </div>
+      </div>
+    
   );
 }
 
